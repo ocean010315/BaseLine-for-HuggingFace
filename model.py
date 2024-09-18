dda079ca42ed6645dfdcdc386211133301777308
오수현 <elena0315@naver.com>
2024-09-19 04:55:17 +0900

    feat: training code

---
+++ model.py
@@ -0,0 +1,43 @@
+import numpy as np
+from scipy.stats import pearsonr
+from transformers import AutoModelForSequenceClassification, TrainingArguments, Trainer
+from torch import optim
+
+
+class Model():
+    def __init__(self, model_name, epoch, train_data, valid_data, lr, weight_decay):
+        self.model = AutoModelForSequenceClassification.from_pretrained(model_name)
+    
+        self.training_arguments = TrainingArguments(
+            output_dir='./results/',
+            overwrite_output_dir=True,
+            num_train_epochs=epoch,
+            learning_rate=lr,
+            weight_decay=weight_decay,
+            eval_strategy="epoch",
+        )
+
+        # optimizer = optim.AdamW(self.model.parameters(), lr=lr, weight_decay=weight_decay)
+        # schedular = optim.lr_schedular.StepLR(optimizer, step_size=1, gamma=0.1)
+
+        self.trainer = Trainer(
+            model=self.model,
+            args=self.training_arguments,
+            train_dataset=train_data,
+            eval_dataset=valid_data,
+            compute_metrics=self.compute_metrics,
+            # optimizers=(optimizer, schedular), # default = AdamW
+        )
+
+    def compute_metrics(self, eval_pred):
+        logits, labels = eval_pred
+        
+        predictions = np.argmax(logits, axis=-1)
+        labels = np.array(labels)
+
+        pearson_corr, _ = pearsonr(predictions, labels)
+
+        return {"pearson_corr": pearson_corr}
+
+    def train(self):
+        self.trainer.train()
