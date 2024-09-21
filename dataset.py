b093587e63e2a4e8669723d6256a6e3d6a3deb22
오수현 <elena0315@naver.com>
2024-09-21 16:17:59 +0900

    fix: dict architecture

--- dataset.py
+++ dataset.py
@@ -42,23 +42,12 @@ class STSDataset(Dataset):
         return len(self.inputs['input_ids'])
     
     def __getitem__(self, idx):
-        # if len(self.labels)==0: # test dataset에는 label이 주어지지 않음
-        #     return {"input_ids": torch.tensor(self.inputs["input_ids"][idx]),
-        #             "token_type_ids": torch.tensor(self.inputs["token_type_ids"][idx]),
-        #             "attention_mask": torch.tensor(self.inputs["attention_mask"][idx])}
+        if len(self.labels)==0: # test dataset에는 label이 주어지지 않음
+            return {"input_ids": torch.tensor(self.inputs["input_ids"][idx]),
+                    "token_type_ids": torch.tensor(self.inputs["token_type_ids"][idx]),
+                    "attention_mask": torch.tensor(self.inputs["attention_mask"][idx])}
         
-        # return {"input_ids": torch.tensor(self.inputs["input_ids"][idx]),
-        #         "token_type_ids": torch.tensor(self.inputs["token_type_ids"][idx]),
-        #         "attention_mask": torch.tensor(self.inputs["attention_mask"][idx]),
-        #         "labels": torch.tensor(self.labels[idx])}
-
-        item = {
-            "input_ids": torch.tensor(self.inputs["input_ids"][idx]),
-            "token_type_ids": torch.tensor(self.inputs["token_type_ids"][idx]),
-            "attention_mask": torch.tensor(self.inputs["attention_mask"][idx])
-        }
-
-        if self.labels is not None:
-            item["labels"] = torch.tensor(self.labels[idx])
-        
-        return item
+        return {"input_ids": torch.tensor(self.inputs["input_ids"][idx]),
+                "token_type_ids": torch.tensor(self.inputs["token_type_ids"][idx]),
+                "attention_mask": torch.tensor(self.inputs["attention_mask"][idx]),
+                "labels": torch.tensor(self.labels[idx])}
