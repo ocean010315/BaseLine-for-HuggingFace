f79c1aa2a6c1a8f16d3ddfd7133e410d31726d75
오수현 <elena0315@naver.com>
2024-09-21 17:13:17 +0900

    fix: typo

--- test.py
+++ test.py
@@ -4,18 +4,19 @@ from torch.utils.data import DataLoader
 import torch
 from pandas import read_csv
 import os
+from tqdm import tqdm
 
 from dataset import preprocess
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--data_path", type=str, default='./test.csv', help="path to test.csv")
-    parser.add_argument("--model_path", type=str, defualt='./results/checkpoint-584', help="dir path to safetensors")
+    parser.add_argument("--model_path", type=str, default='./results/checkpoint-584', help="dir path to safetensors")
     arg = parser.parse_args()
 
     model = AutoModelForSequenceClassification.from_pretrained(arg.model_path)
 
-    test_data = preprocess(arg.data_path)
+    test_data = preprocess(task="test", data_path=arg.data_path, model_name='klue/roberta-small')
     test_loader = DataLoader(test_data, shuffle=False)
 
     device = 'cuda' if torch.cuda.is_available else 'cpu'
